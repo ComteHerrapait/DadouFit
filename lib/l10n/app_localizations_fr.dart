550e417d8ca707f03855f263a849e131117b2820
Comteherrapait <leondecheux@gmail.com>
2025-07-25 01:20:07 +0200

    updated dependencies

---
+++ lib/l10n/app_localizations_fr.dart
@@ -0,0 +1,47 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for French (`fr`).
+class AppLocalizationsFr extends AppLocalizations {
+  AppLocalizationsFr([String locale = 'fr']) : super(locale);
+
+  @override
+  String get welcomeFitBoy => 'Bienvenue, FitBoy !';
+
+  @override
+  String get filterLabel => 'Filtres';
+
+  @override
+  String get doinsportApiWarning =>
+      'Cette application utilise les APIs de doinsport.club';
+
+  @override
+  String get generatedByDallE => 'générée avec DALL·E';
+
+  @override
+  String get defaultValue => 'valeur par défaut';
+
+  @override
+  String get settingsTitle => 'Réglages';
+
+  @override
+  String get settingsDarkMode => 'Mode sombre';
+
+  @override
+  String get settingsColor => 'Couleur';
+
+  @override
+  String get settingsColorPick => 'Choisir couleur';
+
+  @override
+  String get settingsLocale => 'Langue';
+
+  @override
+  String get close => 'Fermer';
+
+  @override
+  String get open => 'Ouvrir';
+}
