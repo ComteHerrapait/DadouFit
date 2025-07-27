9c04c4f874dfec2835881db9daf80a4e5cd8a585
Comteherrapait <leondecheux@gmail.com>
2025-07-27 02:10:18 +0200

    #8 Store settings to shared preferences

--- lib/src/providers/settings_provider.dart
+++ lib/src/providers/settings_provider.dart
@@ -1,40 +1,103 @@
+import 'dart:developer';
+
+import 'package:dadoufit/l10n/app_localizations.dart';
 import 'package:dadoufit/src/js/javascript.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+enum PersistedSettings {
+  darkMode(key: "darkmode_enabled"),
+  locale(key: "locale"),
+  color(key: "theme_seed_color");
+
+  const PersistedSettings({required this.key});
+  final String key;
+}
+
 class SettingsProvider extends ChangeNotifier {
+  late final SharedPreferences prefs;
+  final js = Javascript();
+
   ThemeMode themeMode = ThemeMode.dark;
   Color color = Color.fromARGB(125, 255, 50, 255);
   Locale? locale; // null -> automatic detection
-  final js = Javascript();
 
   SettingsProvider() {
     _init();
   }
 
   void _init() async {
-    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    bool? isDarkTheme = sharedPreferences.getBool("is_dark");
+    prefs = await SharedPreferences.getInstance();
+
+    _loadDarkModeFromPrefs();
+    _loadLocaleFromPrefs();
+    _loadColorFromPrefs();
+
+    notifyListeners();
+  }
+
+  // Load data from persisted preferences
+
+  void _loadDarkModeFromPrefs() {
+    bool? isDarkTheme = prefs.getBool(PersistedSettings.darkMode.key);
     if (isDarkTheme != null && isDarkTheme) {
       themeMode = ThemeMode.dark;
     } else {
       themeMode = ThemeMode.light;
     }
-    notifyListeners();
   }
+
+  void _loadLocaleFromPrefs() {
+    String? localeId = prefs.getString(PersistedSettings.locale.key);
+    if (localeId != null && localeId.isNotEmpty) {
+      Locale fromPrefs = Locale(localeId);
+      if (AppLocalizations.supportedLocales.contains(fromPrefs)) {
+        locale = fromPrefs;
+        return;
+      } else {
+        log("Unauthaurozed locale: $localeId");
+      }
+    }
+    locale = null;
+    prefs.remove(PersistedSettings.locale.key);
+  }
+
+  void _loadColorFromPrefs() {
+    String? colorHex = prefs.getString(PersistedSettings.color.key);
+    if (colorHex != null && colorHex.isNotEmpty) {
+      // Load color from an HEX string
+      Color fromPrefs = Color(int.parse(colorHex, radix: 16));
+      color = fromPrefs;
+    }
+  }
+
+  // Public methods to change preferences values
 
   void changeTheme(ThemeMode newTheme) async {
     themeMode = newTheme;
-    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    sharedPreferences.setBool("is_dark", themeMode == ThemeMode.dark);
+    prefs.setBool(PersistedSettings.darkMode.key, themeMode == ThemeMode.dark);
     notifyListeners();
   }
 
   void changeColor(Color newColor) async {
     color = newColor;
     js.setMetaThemeColor(theme().primaryColor);
+    prefs.setString(PersistedSettings.color.key, newColor.toHexString());
     notifyListeners();
   }
+
+  void changeLocale(Locale? newLocale) async {
+    locale = newLocale;
+    if (newLocale != null) {
+      prefs.setString(PersistedSettings.locale.key, newLocale.toLanguageTag());
+    } else {
+      prefs.remove(PersistedSettings.locale.key);
+    }
+    notifyListeners();
+  }
+
+  // Public getters to read preferences values
 
   bool get isDarkMode => themeMode == ThemeMode.dark;
   Brightness get brightness =>
@@ -46,10 +109,5 @@ class SettingsProvider extends ChangeNotifier {
         seedColor: color,
       ),
     );
-  }
-
-  void changeLocale(Locale? newLocale) async {
-    locale = newLocale;
-    notifyListeners();
   }
 }
