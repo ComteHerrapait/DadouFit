95a6947eaa58bc3338c178d7d3ff74fac91cde80
Léon Delcroix <leondecheux@gmail.com>
2025-09-06 01:10:13 +0200

    Cloudflare config (#11)
    * added install script
    
    * chmod
    
    * remove outdate param
    
    * chmod from mac

---
+++ cloudflare.sh
@@ -0,0 +1,20 @@
+set -e # Fail Fast
+
+if [ -d flutter ]; then
+  (cd flutter && git pull)
+else
+  git clone https://github.com/flutter/flutter.git
+  (cd flutter && git fetch --tags && git switch stable)
+fi
+
+echo "FLUTTER IS INSTALLED"
+
+flutter/bin/flutter doctor
+flutter/bin/flutter clean
+flutter/bin/flutter config --enable-web
+flutter/bin/flutter pub get
+flutter/bin/flutter precache --web
+
+echo "FLUTTER IS SET UP"
+
+flutter/bin/flutter build web --base-href="/" --release
